5241a033a11496b72a4b246fa2e4fea939c31653
FernandaSzAdr <fsandrade25@gmail.com>
2019-04-24 19:02:28 -0300

    Realiza capturua dos valores informados pelo usuario

--- main.asm
+++ main.asm
@@ -8,3 +8,23 @@
 #	* Ordenar os elementos do vetor em ordem crescente
 #	* Escrever na saída (console) o vetor ordenado. 
 
+.data
+    vetor: .space 60
+.text
+la $s1, vetor        		# armazena o endereço do vetor de entrada em $s1			
+li $t0, 0        		# i = 0, interador que sera usado para percorrer o vetor
+
+For: sll $t1, $t0, 2     	# $t1 = i * 4
+add $t2, $t1, $s1    		# $t2 = endreço de vetor[i]
+jal captura_valor    		# chama função para pegar valor, que sera armazenado em $v0
+sw $v0, 0($t2)       		# vetor[i] = $v0
+addi $t0, $t0, 1    		# i = i + 1
+bne $t0, 15, For    		# enquanto i != 15 continue
+j exit_input
+
+captura_valor:
+li $v0, 5        
+syscall
+jr $ra
+
+exit_input:
