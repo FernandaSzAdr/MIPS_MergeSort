655a9a24fce45d1b2af18576a0844f65e4d0f268
FernandaSzAdr <fsandrade25@gmail.com>
2019-04-24 18:46:58 -0300

    Adiciona descrição do projeto no main.asm

---
+++ main.asm
@@ -0,0 +1,10 @@
+# Descrição do projeto:
+# Ordenação de vetores com o Merge Sort
+# Esteprojeto consiste no desenvolvimento de um programa que implemente o método 
+# de ordenação Merge Sort para ser aplicado na ordenação de vetores. 
+# Neste aspecto, o programa deve:
+# 	* Ler o tamanho do vetor informado pelo usuário (admitir pelo menos 15 valores)
+#	* Ler valores de um vetor inteiro fornecidos pelo usuário.
+#	* Ordenar os elementos do vetor em ordem crescente
+#	* Escrever na saída (console) o vetor ordenado. 
+
